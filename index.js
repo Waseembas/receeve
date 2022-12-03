27981f6197792d7feffe84adf999f511811e3563
Waseembas <47210393+Waseembas@users.noreply.github.com>
2022-12-04 02:36:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({region: 'us-west-2'});
+AWS.config.update({region: 'us-east-1'});
 
 
 
